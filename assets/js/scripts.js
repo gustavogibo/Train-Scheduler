b5bdb974406a7d279f0484bc330d8c0067570b16
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-05-09 22:16:18 -0700

    finishing with the logic on calculating the time

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,26 +19,21 @@ $("#btn-register-train").on("click", function(event) {
     // Grabs user input
     var trainName = $("#train-name").val().trim();
     var TrainDestination = $("#train-destination").val().trim();
-    var trainFirst = moment($("#train-first-time").val().trim(), "HH:mm")
+    var trainFirst = moment($("#train-first-time").val().trim(), "HH:mm");
     var trainFrequency = $("#train-frequency").val().trim();
   
     // Creates local "temporary" object for holding employee data
     var newTrain = {
       name: trainName,
       destination: TrainDestination,
-      firstTime: trainFirst,
+      firstTime: trainFirst._i,
       frequency: trainFrequency
-    };
+    };  
   
     // Uploads employee data to the database
     database.ref().push(newTrain);
     
-    console.log(newTrain);
-    // Logs everything to console
-    // console.log(newTrain.name);
-    // console.log(newTrain.destination);
-    // console.log(newTrain.firstTime);
-    // console.log(newTrain.frequency);
+    
   
     // Alert
     alert("Train successfully added");
@@ -49,4 +44,62 @@ $("#btn-register-train").on("click", function(event) {
     $("#train-first-time").val("");
     $("#train-frequency").val("");
   
+});
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into variables.
+  var trainName = childSnapshot.val().name;
+  var TrainDestination = childSnapshot.val().destination;
+  var trainFirst = childSnapshot.val().firstTime;
+  var trainFrequency = childSnapshot.val().frequency;
+
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(trainFirst, "HH:mm").subtract(1, "years");
+  // Current Time
+  var currentTime = moment();
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+
+  // Time apart (remainder)
+  var tRemainder = parseInt(diffTime % trainFrequency);
+
+  // Minute Until Train
+  var tMinutesTillTrain = parseInt(trainFrequency - tRemainder);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  nextTrain = moment(nextTrain).format("hh:mm");
+
+
+  var trainList = $("#train-list");
+  var tr = $("<tr>");
+
+  var tdTrainName = $("<td>");
+  tdTrainName.append(trainName);
+
+  var tdDestination = $("<td>");
+  tdDestination.append(TrainDestination);
+
+  var tdFrequency = $("<td>");
+  tdFrequency.append(trainFrequency);
+
+  var tdFirstTime = $("<td>");
+  tdFirstTime.append(nextTrain);
+
+  var tdMinutesAway = $("<td>");
+  tdMinutesAway.append(tMinutesTillTrain);
+
+  tr.append(tdTrainName);
+  tr.append(tdDestination);
+  tr.append(tdFrequency);
+  tr.append(tdFirstTime);
+  tr.append(tdMinutesAway);
+
+  trainList.append(tr);
 });
